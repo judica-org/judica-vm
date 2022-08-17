6eaff6625c6f8cf0f90f5adc8ccd95098b6a47be
Jeremy Rubin <j@rubin.io>
2022-08-16 17:36:10 -0700

    Move NFT Module to file

--- mine-with-friends-board/src/lib.rs
+++ mine-with-friends-board/src/lib.rs
@@ -1,9 +1,6 @@
 use erc20::{ERC20Ptr, ERC20Registry, ERC20Standard};
 use serde::{ser::SerializeSeq, Serialize};
-use std::{
-    collections::btree_map::*,
-    ops::{Index, IndexMut},
-};
+use std::collections::btree_map::*;
 
 mod erc20;
 #[derive(Eq, Ord, PartialEq, PartialOrd, Clone, Serialize, Copy)]
@@ -21,138 +18,7 @@ impl ContractCreator {
     }
 }
 
-trait NFT {
-    fn owner(&self) -> UserID;
-    fn transfer(&mut self, to: UserID);
-    fn id(&self) -> UserID;
-    fn transfer_count(&self) -> u128;
-}
-
-type Price = u128;
-type Currency = ERC20Ptr;
-type NFTID = UserID;
-struct NFTRegistry {
-    nfts: BTreeMap<NftPtr, Box<dyn NFT>>,
-}
-#[derive(Serialize, Eq, Ord, PartialEq, PartialOrd, Clone, Copy)]
-struct NftPtr(UserID);
-impl NFTRegistry {
-    fn add(&mut self, nft: Box<dyn NFT>) -> NftPtr {
-        let id = NftPtr(nft.id());
-        if self.nfts.contains_key(&id) {
-        } else {
-            self.nfts.insert(id.clone(), nft);
-        }
-        id
-    }
-}
-
-impl Index<NftPtr> for NFTRegistry {
-    type Output = Box<dyn NFT>;
-
-    fn index(&self, index: NftPtr) -> &Self::Output {
-        self.nfts.get(&index).unwrap()
-    }
-}
-impl IndexMut<NftPtr> for NFTRegistry {
-    fn index_mut(&mut self, index: NftPtr) -> &mut Self::Output {
-        self.nfts.get_mut(&index).unwrap()
-    }
-}
-
-struct NFTSaleRegistry {
-    nfts: BTreeMap<NftPtr, (Price, Currency, UserID, u128)>,
-}
-impl NFTSaleRegistry {
-    fn list_nft(&mut self, asset: NftPtr, price: Price, currency: Currency, nfts: &NFTRegistry) {
-        self.nfts.insert(
-            asset,
-            (
-                price,
-                currency,
-                nfts[asset].owner(),
-                nfts[asset].transfer_count(),
-            ),
-        );
-    }
-    fn make_trade(
-        &mut self,
-        to: UserID,
-        asset: NftPtr,
-        tokens: &mut ERC20Registry,
-        nfts: &mut NFTRegistry,
-        limit_price: Price,
-        limit_currency: Currency,
-    ) {
-        if let Some((price, currency, who, transfer_count)) = self.nfts.get(&asset) {
-            if *who != nfts[asset.clone()].owner() {
-                return;
-            }
-            if *transfer_count != nfts[asset.clone()].transfer_count() {
-                return;
-            }
-            if limit_currency != *currency {
-                return;
-            }
-            if limit_price >= *price {
-                return;
-            }
-            let token = &mut tokens[currency.clone()];
-            token.transaction();
-            if token.transfer(&to, &nfts[asset.clone()].owner(), *price) {
-                nfts[asset].transfer(to);
-            }
-            token.end_transaction();
-        }
-    }
-}
-
-struct BaseNFT {
-    owner: UserID,
-    plant_id: UserID,
-    transfer_count: u128,
-}
-
-impl NFT for BaseNFT {
-    fn owner(&self) -> UserID {
-        self.owner
-    }
-
-    fn transfer(&mut self, to: UserID) {
-        self.owner = to;
-        self.transfer_count += 1;
-    }
-
-    fn id(&self) -> UserID {
-        self.plant_id
-    }
-
-    fn transfer_count(&self) -> u128 {
-        self.transfer_count
-    }
-}
-
-struct PowerPlant {
-    base: BaseNFT,
-}
-
-impl NFT for PowerPlant {
-    fn owner(&self) -> UserID {
-        self.base.owner()
-    }
-
-    fn transfer(&mut self, to: UserID) {
-        self.base.transfer(to)
-    }
-
-    fn id(&self) -> UserID {
-        self.base.id()
-    }
-
-    fn transfer_count(&self) -> u128 {
-        self.base.transfer_count()
-    }
-}
+mod nft;
 
 struct GameBoard {
     erc20s: erc20::ERC20Registry,
@@ -161,8 +27,8 @@ struct GameBoard {
     turn_count: u64,
     alloc: ContractCreator,
     users: BTreeMap<UserID, String>,
-    nfts: NFTRegistry,
-    nft_sales: NFTSaleRegistry,
+    nfts: nft::NFTRegistry,
+    nft_sales: nft::NFTSaleRegistry,
     power_plants: (),
     player_move_sequence: BTreeMap<UserID, u64>,
     new_users_allowed: bool,
@@ -205,7 +71,7 @@ impl GameBoard {
                         .insert(self.erc20s.new_token(Box::new(ERC20Standard::default())));
                     self.root_user.insert(self.alloc.make());
                     // TODO: Initialize Power Plants?
-                    let demo_nft = self.nfts.add(Box::new(BaseNFT {
+                    let demo_nft = self.nfts.add(Box::new(nft::BaseNFT {
                         owner: self.root_user.unwrap(),
                         plant_id: self.alloc.make(),
                         transfer_count: 0,
@@ -249,8 +115,8 @@ enum GameMove {
     Init,
     NoNewUsers,
     Trade(token_swap::PairID, u128, u128),
-    PurchaseNFT(NftPtr, Price, Currency),
-    ListNFTForSale(NftPtr, Price, Currency),
+    PurchaseNFT(nft::NftPtr, nft::Price, nft::Currency),
+    ListNFTForSale(nft::NftPtr, nft::Price, nft::Currency),
     RegisterUser(String),
 }
 
---
+++ mine-with-friends-board/src/nft.rs
@@ -0,0 +1,157 @@
+use crate::erc20::ERC20Registry;
+
+use std::ops::IndexMut;
+
+use std::ops::Index;
+
+use crate::erc20::ERC20Ptr;
+
+use super::UserID;
+use std::collections::BTreeMap;
+use serde::Serialize;
+
+pub(crate) trait NFT {
+    fn owner(&self) -> UserID;
+    fn transfer(&mut self, to: UserID);
+    fn id(&self) -> UserID;
+    fn transfer_count(&self) -> u128;
+}
+
+pub(crate) type Price = u128;
+
+pub(crate) type Currency = ERC20Ptr;
+
+pub(crate) type NFTID = UserID;
+
+pub(crate) struct NFTRegistry {
+    pub(crate) nfts: BTreeMap<NftPtr, Box<dyn NFT>>,
+}
+
+#[derive(Serialize, Eq, Ord, PartialEq, PartialOrd, Clone, Copy)]
+pub(crate) struct NftPtr(UserID);
+
+impl NFTRegistry {
+    pub(crate) fn add(&mut self, nft: Box<dyn NFT>) -> NftPtr {
+        let id = NftPtr(nft.id());
+        if self.nfts.contains_key(&id) {
+        } else {
+            self.nfts.insert(id.clone(), nft);
+        }
+        id
+    }
+}
+
+impl Index<NftPtr> for NFTRegistry {
+    type Output = Box<dyn NFT>;
+
+    fn index(&self, index: NftPtr) -> &Self::Output {
+        self.nfts.get(&index).unwrap()
+    }
+}
+
+impl IndexMut<NftPtr> for NFTRegistry {
+    fn index_mut(&mut self, index: NftPtr) -> &mut Self::Output {
+        self.nfts.get_mut(&index).unwrap()
+    }
+}
+
+pub(crate) struct NFTSaleRegistry {
+    pub(crate) nfts: BTreeMap<NftPtr, (Price, Currency, UserID, u128)>,
+}
+
+impl NFTSaleRegistry {
+    pub(crate) fn list_nft(
+        &mut self,
+        asset: NftPtr,
+        price: Price,
+        currency: Currency,
+        nfts: &NFTRegistry,
+    ) {
+        self.nfts.insert(
+            asset,
+            (
+                price,
+                currency,
+                nfts[asset].owner(),
+                nfts[asset].transfer_count(),
+            ),
+        );
+    }
+    pub(crate) fn make_trade(
+        &mut self,
+        to: UserID,
+        asset: NftPtr,
+        tokens: &mut ERC20Registry,
+        nfts: &mut NFTRegistry,
+        limit_price: Price,
+        limit_currency: Currency,
+    ) {
+        if let Some((price, currency, who, transfer_count)) = self.nfts.get(&asset) {
+            if *who != nfts[asset.clone()].owner() {
+                return;
+            }
+            if *transfer_count != nfts[asset.clone()].transfer_count() {
+                return;
+            }
+            if limit_currency != *currency {
+                return;
+            }
+            if limit_price >= *price {
+                return;
+            }
+            let token = &mut tokens[currency.clone()];
+            token.transaction();
+            if token.transfer(&to, &nfts[asset.clone()].owner(), *price) {
+                nfts[asset].transfer(to);
+            }
+            token.end_transaction();
+        }
+    }
+}
+
+pub(crate) struct BaseNFT {
+    pub(crate) owner: UserID,
+    pub(crate) plant_id: UserID,
+    pub(crate) transfer_count: u128,
+}
+
+impl NFT for BaseNFT {
+    fn owner(&self) -> UserID {
+        self.owner
+    }
+
+    fn transfer(&mut self, to: UserID) {
+        self.owner = to;
+        self.transfer_count += 1;
+    }
+
+    fn id(&self) -> UserID {
+        self.plant_id
+    }
+
+    fn transfer_count(&self) -> u128 {
+        self.transfer_count
+    }
+}
+
+pub(crate) struct PowerPlant {
+    pub(crate) base: BaseNFT,
+}
+
+impl NFT for PowerPlant {
+    fn owner(&self) -> UserID {
+        self.base.owner()
+    }
+
+    fn transfer(&mut self, to: UserID) {
+        self.base.transfer(to)
+    }
+
+    fn id(&self) -> UserID {
+        self.base.id()
+    }
+
+    fn transfer_count(&self) -> u128 {
+        self.base.transfer_count()
+    }
+}
