b9588fbdf87baf9edb8c6f40bd4128407bb191a4
Jeremy Rubin <j@rubin.io>
2022-08-20 12:51:28 -0700

    Factor out tasks into startup functions

--- ux/src-tauri/src/main.rs
+++ ux/src-tauri/src/main.rs
@@ -35,7 +35,10 @@ use tauri::{
     State, Window,
 };
 use tokio::{
-    sync::{mpsc::unbounded_channel, Notify, OnceCell},
+    sync::{
+        mpsc::{unbounded_channel, UnboundedReceiver},
+        Notify, OnceCell,
+    },
     time::sleep,
 };
 
@@ -148,136 +151,29 @@ fn main() {
     let game = Arc::new(Mutex::new(Some(GameBoard::new())));
     let g = Game(game, Arc::new(Notify::new()));
     let db = Database(OnceCell::new());
-    let (mut tx, mut rx) = unbounded_channel();
     /// Goes through the oracles commitments in order
-    {
-        let db = db.clone();
-        spawn(async move {
-            let oracle_key = get_oracle_key();
-            let mut count = 0;
-            loop {
-                let db = db.get().await.unwrap();
-                'check: loop {
-                    let msg = {
-                        let handle = db.get_handle().await;
-                        handle.get_message_at_height_for_user(oracle_key, count)
-                    };
-                    match msg {
-                        Ok(envelope) => {
-                            match serde_json::from_value::<VecDeque<CanonicalEnvelopeHash>>(
-                                envelope.msg,
-                            ) {
-                                Ok(v) => {
-                                    if tx.send(v).is_err() {
-                                        return;
-                                    };
-                                    count += 1;
-                                }
-                                Err(_) => {
-                                    return;
-                                }
-                            }
-                            break 'check;
-                        }
-                        Err(_) => {
-                            sleep(Duration::from_secs(10)).await;
-                        }
-                    }
-                }
-            }
-        });
-    }
+    let (sequencer_reader_task, output_envelope_hashes) = { start_sequencer(&db) };
 
     /// This task builds a HashMap of all unprocessed envelopes regularly
-    let mut shared_envelopes =
-        Arc::new(Mutex::new(HashMap::<CanonicalEnvelopeHash, Envelope>::new()));
-    let notify_new_envelopes = Arc::new(Notify::new());
-    {
-        let envelopes = shared_envelopes.clone();
-        let db = db.clone();
-        let notify = notify_new_envelopes.clone();
-        spawn(async move {
-            let mut newer = None;
-            loop {
-                let newer_before = newer;
-                {
-                    let db = db.get().await.unwrap();
-                    let handle = db.get_handle().await;
-                    let mut env = envelopes.lock().await;
-                    handle.get_all_messages_collect_into_inconsistent(&mut newer, &mut env);
-                }
-
-                if newer_before != newer {
-                    notify.notify_waiters();
-                }
-                sleep(Duration::from_secs(10)).await;
-            }
-        });
-    }
+    let (shared_envelopes, notify_new_envelopes, shared_envelopes_task) =
+        { start_envelope_db_fetcher(&db) };
     // Whenever new sequencing comes in, wait until they are all in the messages DB, and then drain them out for processing
-    let (mut tx_envelopes_to_process, mut rx_envelopes_to_process) = unbounded_channel();
-    {
-        let envelopes = shared_envelopes.clone();
-        let notify = notify_new_envelopes.clone();
-        spawn(async move {
-            loop {
-                if let Some(mut envelope_hashes) = rx.recv().await {
-                    let mut should_wait = None;
-                    'wait_for_new: while envelope_hashes.len() != 0 {
-                        if let Some(n) = should_wait.take() {
-                            // register for notification, then drop lock, then wait
-                            n.await;
-                        }
-                        let mut envs = envelopes.lock().await;
-                        while let Some(envelope) = envelope_hashes.pop_front() {
-                            match envs.entry(envelope) {
-                                Occupied(e) => {
-                                    // TODO: Batch size
-                                    tx_envelopes_to_process.send(e.remove());
-                                }
-                                Vacant(k) => {
-                                    envelope_hashes.push_front(k.into_key());
-                                    should_wait.insert(notify.notified());
-                                    break 'wait_for_new;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        });
-    }
+    let (envelope_batcher, output_batches) = {
+        start_envelope_batcher(
+            shared_envelopes,
+            notify_new_envelopes,
+            output_envelope_hashes,
+        )
+    };
     // Run the deserialization of the inner message type to move sets in it's own thread so that we can process
     // moves in a pipeline as they get deserialized
     // TODO: We skip invalid moves? Should do something else?
-    let (mut tx2, mut rx2) = unbounded_channel();
-    {
-        spawn(async move {
-            while let Some(envelope) = rx_envelopes_to_process.recv().await {
-                let r_game_move = serde_json::from_value(envelope.msg);
-                match r_game_move {
-                    Ok(game_move) => {
-                        if tx2.send((game_move, envelope.header.key.to_hex())).is_err() {
-                            return;
-                        }
-                    }
-                    Err(_) => {}
-                }
-            }
-        });
-    }
+    let (move_deserializer, output_moves) = { start_move_deserializer(output_batches) };
     // Play the moves one by one
-    {
+    let game_task = {
         let g = g.clone();
-        spawn(async move {
-            while let Some((game_move, s)) = rx2.recv().await {
-                let mut game = g.0.lock().await;
-                game.as_mut().unwrap().play(game_move, s);
-                // TODO: Maybe notify less often?
-                g.1.notify_waiters();
-            }
-        });
-    }
+        start_game(g, output_moves)
+    };
 
     tauri::Builder::default()
         .setup(|app| Ok(()))
@@ -291,4 +187,158 @@ fn main() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn start_sequencer(
+    db: &Database,
+) -> (
+    tauri::async_runtime::JoinHandle<()>,
+    tokio::sync::mpsc::UnboundedReceiver<VecDeque<CanonicalEnvelopeHash>>,
+) {
+    let (mut tx, mut rx) = unbounded_channel();
+    let db = db.clone();
+    let task = spawn(async move {
+        let oracle_key = get_oracle_key();
+        let mut count = 0;
+        loop {
+            let db = db.get().await.unwrap();
+            'check: loop {
+                let msg = {
+                    let handle = db.get_handle().await;
+                    handle.get_message_at_height_for_user(oracle_key, count)
+                };
+                match msg {
+                    Ok(envelope) => {
+                        match serde_json::from_value::<VecDeque<CanonicalEnvelopeHash>>(
+                            envelope.msg,
+                        ) {
+                            Ok(v) => {
+                                if tx.send(v).is_err() {
+                                    return;
+                                };
+                                count += 1;
+                            }
+                            Err(_) => {
+                                return;
+                            }
+                        }
+                        break 'check;
+                    }
+                    Err(_) => {
+                        sleep(Duration::from_secs(10)).await;
+                    }
+                }
+            }
+        }
+    });
+    (task, rx)
+}
+
+fn start_envelope_db_fetcher(
+    db: &Database,
+) -> (
+    Arc<Mutex<HashMap<CanonicalEnvelopeHash, Envelope>>>,
+    Arc<Notify>,
+    tauri::async_runtime::JoinHandle<()>,
+) {
+    let shared_envelopes = Arc::new(Mutex::new(HashMap::<CanonicalEnvelopeHash, Envelope>::new()));
+    let notify_new_envelopes = Arc::new(Notify::new());
+    let envelopes = shared_envelopes.clone();
+    let db = db.clone();
+    let notify = notify_new_envelopes.clone();
+    let task = spawn(async move {
+        let mut newer = None;
+        loop {
+            let newer_before = newer;
+            {
+                let db = db.get().await.unwrap();
+                let handle = db.get_handle().await;
+                let mut env = envelopes.lock().await;
+                handle.get_all_messages_collect_into_inconsistent(&mut newer, &mut env);
+            }
+
+            if newer_before != newer {
+                notify.notify_waiters();
+            }
+            sleep(Duration::from_secs(10)).await;
+        }
+    });
+    (shared_envelopes, notify_new_envelopes, task)
+}
+
+fn start_envelope_batcher(
+    shared_envelopes: Arc<Mutex<HashMap<CanonicalEnvelopeHash, Envelope>>>,
+    notify_new_envelopes: Arc<Notify>,
+    mut input_envelope_hashers: UnboundedReceiver<VecDeque<CanonicalEnvelopeHash>>,
+) -> (
+    tauri::async_runtime::JoinHandle<()>,
+    tokio::sync::mpsc::UnboundedReceiver<Envelope>,
+) {
+    let (mut tx_envelopes_to_process, mut rx_envelopes_to_process) = unbounded_channel();
+    let envelopes = shared_envelopes.clone();
+    let notify = notify_new_envelopes.clone();
+    let task = spawn(async move {
+        while let Some(mut envelope_hashes) = input_envelope_hashers.recv().await {
+            let mut should_wait = None;
+            'wait_for_new: while envelope_hashes.len() != 0 {
+                if let Some(n) = should_wait.take() {
+                    // register for notification, then drop lock, then wait
+                    n.await;
+                }
+                let mut envs = envelopes.lock().await;
+                while let Some(envelope) = envelope_hashes.pop_front() {
+                    match envs.entry(envelope) {
+                        Occupied(e) => {
+                            // TODO: Batch size
+                            tx_envelopes_to_process.send(e.remove());
+                        }
+                        Vacant(k) => {
+                            envelope_hashes.push_front(k.into_key());
+                            should_wait.insert(notify.notified());
+                            break 'wait_for_new;
+                        }
+                    }
+                }
+            }
+        }
+    });
+    (task, rx_envelopes_to_process)
+}
+
+fn start_move_deserializer(
+    mut input_envelopes: tokio::sync::mpsc::UnboundedReceiver<Envelope>,
+) -> (
+    tauri::async_runtime::JoinHandle<()>,
+    tokio::sync::mpsc::UnboundedReceiver<(MoveEnvelope, String)>,
+) {
+    let (mut tx2, mut rx2) = unbounded_channel();
+    let task = spawn(async move {
+        while let Some(envelope) = input_envelopes.recv().await {
+            let r_game_move = serde_json::from_value(envelope.msg);
+            match r_game_move {
+                Ok(game_move) => {
+                    if tx2.send((game_move, envelope.header.key.to_hex())).is_err() {
+                        return;
+                    }
+                }
+                Err(_) => {}
+            }
+        }
+    });
+    (task, rx2)
+}
+
+fn start_game(
+    g: Game,
+    mut input_moves: UnboundedReceiver<(MoveEnvelope, String)>,
+) -> tauri::async_runtime::JoinHandle<()> {
+    let task = spawn(async move {
+        while let Some((game_move, s)) = input_moves.recv().await {
+            let mut game = g.0.lock().await;
+            game.as_mut().unwrap().play(game_move, s);
+            // TODO: Maybe notify less often?
+            g.1.notify_waiters();
+        }
+    });
+    task
 }
