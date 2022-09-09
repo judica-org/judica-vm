judica-org/judica-vm

commit a403877b52ab5b3f503b4050307d82e8bf3ddf35

Get Subscription System Properly Working...

common/attest-database/src/db_handle/get/messages.rs
@@ -1,6 +1,7 @@
 use super::super::handle_type;
 use super::super::MsgDBHandle;
 use crate::db_handle::sql::get::messages::*;
+use crate::db_handle::MessageID;
 use attest_messages::Authenticated;
 use attest_messages::CanonicalEnvelopeHash;
 use attest_messages::Envelope;
@@ -187,11 +188,19 @@ where
         I: Iterator<Item = &'i CanonicalEnvelopeHash>,
         E: AsRef<Envelope> + FromSql,
     {
-        let mut stmt = self.0.prepare_cached(SQL_GET_MESSAGE_BY_HASH)?;
+        let mut stmt = self.0.prepare_cached(SQL_GET_MESSAGE_BY_ID)?;
         let r: Result<Vec<_>, _> = hashes
             .map(|hash| stmt.query_row([hash], |r| r.get::<_, E>(0)))
             .collect();
         r
+    }
+
+    pub fn messages_by_id<'i, E>(&self, id: MessageID) -> Result<E, rusqlite::Error>
+    where
+        E: AsRef<Envelope> + FromSql,
+    {
+        let mut stmt = self.0.prepare_cached(SQL_GET_MESSAGE_BY_ID)?;
+        stmt.query_row([id], |r| r.get::<_, E>(0))
     }
 
     pub fn message_exists(&self, hash: &sha256::Hash) -> Result<bool, rusqlite::Error> {

common/attest-database/src/db_handle/insert/mod.rs
@@ -172,6 +172,21 @@ where
         ))?;
         Ok(())
     }
+    /// Add Member to Chain Commit Group
+    pub fn add_subscriber_to_chain_commit_group(
+        &self,
+        group_id: ChainCommitGroupID,
+        genesis_hash: CanonicalEnvelopeHash,
+    ) -> Result<(), rusqlite::Error> {
+        let mut stmt = self
+            .0
+            .prepare_cached(SQL_INSERT_CHAIN_COMMIT_GROUP_SUBSCRIBER)?;
+        let _ = stmt.insert(rusqlite::named_params!(
+            ":genesis_hash": genesis_hash,
+            ":group_id": group_id
+        ))?;
+        Ok(())
+    }
 }
 
 #[must_use]

common/attest-database/src/db_handle/mod.rs
@@ -35,7 +35,7 @@ pub mod handle_type {
 }
 macro_rules! row_type (
     {$RowType:ident} => {
-#[derive(PartialEq, PartialOrd, Ord, Eq, Clone, Copy, Serialize, Deserialize)]
+#[derive(PartialEq, PartialOrd, Ord, Eq, Clone, Copy, Serialize, Deserialize, Debug)]
 pub struct $RowType(i64);
 impl ToSql for $RowType {
     fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {

common/attest-database/src/db_handle/sql/get/chain_commit_groups/all_chain_commit_group_members_for_chain.sql
@@ -1,14 +1,15 @@
-WITH groups_is_in AS (
+WITH subscribing_to AS (
     SELECT
         group_id
     FROM
-        chain_commit_group_members CommitGroup
-        INNER JOIN messages Messages ON Messages.hash = :genesis_hash
-        AND Messages.height = 0
-        AND CommitGroup.member_id = Messages.message_id
+        chain_commit_group_subscribers Subscription
+        INNER JOIN messages Msg ON Subscription.member_id = Msg.message_id
+    WHERE
+        Msg.hash = :genesis_hash
+        AND Msg.height = 0
 )
 SELECT
-    member_id
+    DISTINCT member_id
 FROM
-    chain_commit_group_members GroupMembers
-    INNER JOIN groups_is_in InGroups ON GroupMembers.group_id = InGroups.group_id
+    chain_commit_group_members GroupMember
+    INNER JOIN subscribing_to Subscription ON GroupMember.group_id = Subscription.group_id

common/attest-database/src/db_handle/sql/get/chain_commit_groups/all_chain_commit_group_members_tips_for_chain.sql
@@ -1,23 +1,26 @@
-WITH groups_is_in AS (
+WITH groups_this_chain_subscribes_to AS (
     SELECT
-        CommitGroup.group_id
+        Subscription.group_id
     FROM
-        chain_commit_group_members CommitGroup
-        INNER JOIN users Users
-        INNER JOIN messages Messages ON Messages.user_id = Users.user_id
-        AND CommitGroup.member_id = Messages.message_id
+        users User
+        INNER JOIN chain_commit_group_subscribers Subscription
+        INNER JOIN messages Msg ON Msg.user_id = User.user_id
+        AND Subscription.member_id = Msg.message_id
     WHERE
-        Messages.height = 0
-        AND Users.key = :key
+        User.key = :key
+        AND Msg.height = 0
 )
 SELECT
-    Messages.body,
-    max(Messages.height)
+    Msg.body,
+    max(Msg.height)
 FROM
-    chain_commit_group_members GroupMembers
-    INNER JOIN groups_is_in InGroups ON GroupMembers.group_id = InGroups.group_id
-    INNER JOIN messages Messages ON GroupMembers.member_id = Messages.message_id
+    groups_this_chain_subscribes_to Subscription
+    INNER JOIN chain_commit_group_members GroupMembers ON GroupMembers.group_id = Subscription.group_id
+    INNER JOIN messages Msg ON (
+        GroupMembers.member_id = Msg.message_id
+        OR GroupMembers.member_id = Msg.genesis_id
+    )
 WHERE
-    Messages.connected
+    Msg.connected
 GROUP BY
-    Messages.height
+    Msg.user_id

common/attest-database/src/db_handle/sql/get/chain_commit_groups/all_chain_commit_groups_for_chain.sql
@@ -2,9 +2,9 @@ SELECT
     CommitGroup.group_id,
     CommitGroup.name
 FROM
-    messages Messages
-    INNER JOIN chain_commit_group_members GroupMember ON GroupMember.member_id = Messages.message_id
-    INNER JOIN chain_commit_groups CommitGroup ON CommitGroup.group_id = GroupMember.group_id
+    messages Msg
+    INNER JOIN chain_commit_group_subscribers Subscription ON Subscription.member_id = Msg.message_id
+    INNER JOIN chain_commit_groups CommitGroup ON CommitGroup.group_id = Subscription.group_id
 WHERE
-    Messages.hash = :genesis_hash
-    AND Messages.height = 0
+    Msg.hash = :genesis_hash
+    AND Msg.height = 0

common/attest-database/src/db_handle/sql/get/messages/by_id.sql
@@ -1,0 +1,6 @@
+SELECT
+    M.body
+FROM
+    messages M
+where
+    M.message_id = ?

common/attest-database/src/db_handle/sql/insert/add_chain_commit_group_subscriber.sql
@@ -1,0 +1,15 @@
+INSERT INTO
+    chain_commit_group_subscribers(member_id, group_id)
+VALUES
+    (
+        (
+            SELECT
+                M.message_id
+            FROM
+                messages M
+            WHERE
+                M.hash = :genesis_hash
+            LIMIT
+                1
+        ), :group_id
+    )

commit 4820909b5dc8698caee6c99729686537a07cf0e5

Fix Regression Introduced in a403877b52ab5b3f503b4050307d82e8bf3ddf35

common/attest-database/src/db_handle/get/messages.rs
@@ -188,7 +188,7 @@ where
         I: Iterator<Item = &'i CanonicalEnvelopeHash>,
         E: AsRef<Envelope> + FromSql,
     {
-        let mut stmt = self.0.prepare_cached(SQL_GET_MESSAGE_BY_ID)?;
+        let mut stmt = self.0.prepare_cached(SQL_GET_MESSAGE_BY_HASH)?;
         let r: Result<Vec<_>, _> = hashes
             .map(|hash| stmt.query_row([hash], |r| r.get::<_, E>(0)))
             .collect();