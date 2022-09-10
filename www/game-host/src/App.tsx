77eb6845f1788913ed4fdf6f38aa83070a32d6f0
Jeremy Rubin <j@rubin.io>
2022-09-09 18:55:10 -0700

    Add some components to the host www

--- www/game-host/src/App.tsx
+++ www/game-host/src/App.tsx
@@ -5,22 +5,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListGames></ListGames>
+      <NewGame></NewGame>
     </div>
   );
 }
+type CreatedNewChain = {
+  genesis_hash: string,
+  group_name: string,
+};
+function NewGame() {
+  async function handle_click() {
+    let res = await fetch("http://127.0.0.1:13329/attestation_chain/new",
+      { method: "POST" });
+
+    let js = await res.json() as CreatedNewChain;
+    console.log(js);
+
+  }
+  return <button onClick={handle_click}>New Game</button>
+}
+
+function ListGames() {
+  const [list_of_games, set_list_of_games] = React.useState([]);
+  React.useEffect(() => {
+    let cancel = false;
+    const updater = async () => {
+      if (cancel) return;
+      let res = await fetch("http://127.0.0.1:13329/attestation_chain");
+      let js = await res.json();
+      console.log(js);
+      set_list_of_games(js);
+      setTimeout(() => updater(), 5000);
+    }
+    updater();
+    return () => { cancel = true };
+  }, []);
+  const games = list_of_games.map((m) => <li> {m}</li>);
+  return <div>
+    <ul>
+      {games}
+    </ul>
+  </div>
+}
+
 
 export default App;
