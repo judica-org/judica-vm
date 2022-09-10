c663cbc2716249a1163152a71efb59da0c6c1afe
Jeremy Rubin <j@rubin.io>
2022-09-10 15:52:54 -0700

    Make Game Frontend able to connect to different hosts

--- www/game-host/src/App.tsx
+++ www/game-host/src/App.tsx
@@ -2,6 +2,71 @@ import React, { FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type AddChainToGroupArgs = {
+  group: string,
+  genesis_hash: string
+
+};
+class Client {
+  base_url: string;
+  constructor() {
+    const start = new URL(global.location.toString());
+    this.base_url =
+      start.searchParams.get("service_url") ?? "";
+    if (this.base_url === "")
+      throw "Service URL Required";
+  }
+  async create_new_chain(): Promise<CreatedNewChain> {
+
+    let res = await fetch(`${this.base_url}/attestation_chain/new`,
+      {
+        method: "POST"
+      });
+
+    let js = await res.json() as CreatedNewChain;
+    return js;
+  }
+  async add_chain_to_group(obj: AddChainToGroupArgs): Promise<void> {
+
+    let res = await fetch(`${this.base_url}/attestation_chain/commit_group/add_member`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+      });
+
+    let js = await res.json();
+    console.log(js);
+  }
+  async add_peer(obj: { service_url: string, port: number }): Promise<void> {
+    let res = await fetch(`${this.base_url}/peer`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+      });
+
+    let js = await res.json();
+    console.log(js);
+    return;
+  }
+  async list_games(): Promise<Array<string>> {
+    let res = await fetch(`${this.base_url}/attestation_chain`);
+    let js = await res.json();
+    console.log(js);
+    return js;
+  }
+  async list_peers(): Promise<Array<Peer>> {
+    let res = await fetch(`${this.base_url}/peer`);
+    let js = await res.json();
+    return js;
+  }
+}
+const client = new Client();
 function App() {
   return (
     <div className="App">
@@ -27,12 +92,8 @@ type CreatedNewChain = {
 };
 function NewGame() {
   async function handle_click() {
-    let res = await fetch("http://127.0.0.1:11409/attestation_chain/new",
-      { method: "POST" });
-
-    let js = await res.json() as CreatedNewChain;
+    const js = await client.create_new_chain();
     console.log(js);
-
   }
   return <button onClick={handle_click}>New Game</button>
 }
@@ -45,17 +106,10 @@ function AddPeerToNode() {
       port: { valueAsNumber: number }
     };
     let obj = {
-      service_url: t.service_url,
-      port: t.port,
+      service_url: t.service_url.value,
+      port: t.port.valueAsNumber,
     };
-    let res = await fetch("http://127.0.0.1:11409/peer",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(obj)
-      });
-
-    let js = await res.json();
+    const js = client.add_peer(obj);
     console.log(js);
 
   }
@@ -75,19 +129,11 @@ function AddChainToGroup() {
       genesis_hash: { value: string }
     };
     let obj = {
-      group: t.group,
-      genesis_hash: t.genesis_hash,
+      group: t.group.value,
+      genesis_hash: t.genesis_hash.value,
 
     };
-    let res = await fetch("http://127.0.0.1:11409/attestation_chain/commit_group/add_member",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(obj)
-      });
-
-    let js = await res.json();
-    console.log(js);
+    client.add_chain_to_group(obj);
 
   }
   return <form onSubmit={handle_click}>
@@ -100,14 +146,12 @@ function AddChainToGroup() {
 }
 
 function ListGames() {
-  const [list_of_games, set_list_of_games] = React.useState([]);
+  const [list_of_games, set_list_of_games] = React.useState<Array<string>>([]);
   React.useEffect(() => {
     let cancel = false;
     const updater = async () => {
       if (cancel) return;
-      let res = await fetch("http://127.0.0.1:11409/attestation_chain");
-      let js = await res.json();
-      console.log(js);
+      let js = await client.list_games();
       set_list_of_games(js);
       setTimeout(() => updater(), 1000);
     }
@@ -123,14 +167,14 @@ function ListGames() {
   </div>
 }
 
+type Peer = { service_url: string, port: number };
 function ListPeers() {
-  const [list_of_peers, set_list_of_peers] = React.useState<Array<{ service_url: string, port: number }>>([]);
+  const [list_of_peers, set_list_of_peers] = React.useState<Array<Peer>>([]);
   React.useEffect(() => {
     let cancel = false;
     const updater = async () => {
       if (cancel) return;
-      let res = await fetch("http://127.0.0.1:11409/peer");
-      let js = await res.json();
+      const js = await client.list_peers();
       console.log(js);
       set_list_of_peers(js);
       setTimeout(() => updater(), 1000);
