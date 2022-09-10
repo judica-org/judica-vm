dcaf1c74403d110e88d3a3c97989d824edc55f9c
Jeremy Rubin <j@rubin.io>
2022-09-10 14:39:40 -0700

    Fixes and Cleanups to Startup Script

--- contrib/start_attest_www.sh
+++ contrib/start_attest_www.sh
@@ -2,6 +2,12 @@
 cd ../ux-attest
 export PORT=3002
 export BROWSER=none
-# python3 -m webbrowser http://localhost:3002
-# python3 -m webbrowser http://localhost:3002
+$(
+    while true; do
+        sleep 1 && curl http://localhost:$PORT && break
+    done
+    $(
+        echo $PORTS | xargs -I{} python3 -m webbrowser "http://localhost:3002?service_url=http%3A%2F%2F127.0.0.1%3A{}"
+    )
+) &
 yarn start react
--- contrib/start_tauri.sh
+++ contrib/start_tauri.sh
@@ -3,4 +3,5 @@
 export CONF=$(cat disable_tauri_front.json)
 echo $CONF | jq
 cd ../ux
+sleep 10
 yarn tauri dev -c "$CONF"
