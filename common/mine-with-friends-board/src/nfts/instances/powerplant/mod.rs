284324ad44e8ace99a2891d7c5d82fcb1c2400b1
Caroline Ponzini <cara@Carolines-MacBook-Air.local>
2022-09-14 13:40:20 -0700

    move mint function to PowerPlant mod

--- common/mine-with-friends-board/src/nfts/instances/powerplant/mod.rs
+++ common/mine-with-friends-board/src/nfts/instances/powerplant/mod.rs
@@ -1,10 +1,12 @@
 use std::cmp::min;
 
-use serde::Serialize;
-
 pub mod events;
 use crate::{game::GameBoard, nfts::NftPtr, tokens::TokenPointer, util::Price};
-
+use super::BaseNFT;
+use crate::entity::EntityID;
+use crate::game::GameBoard;
+use crate::util::Currency;
+use crate::util::Price;
 use super::lockup::CoinLockup;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -24,13 +26,58 @@ pub(crate) struct PowerPlant {
 }
 
 impl PowerPlant {
-    /// Create a new PowerPlant NFT
-    pub(crate) fn new(id: NftPtr, plant_type: PlantType, coordinates: (u64, u64)) -> Self {
+    /// Create a new PowerPlant
+    fn new(id: NftPtr, plant_type: PlantType, coordinates: (u64, u64)) -> Self {
         Self {
             id,
             plant_type,
             watts: 1000, // this should probably change
             coordinates,
+        }
+    }
+    /// Mint a new PowerPlant NFT
+    pub(crate) fn mint_power_plant(
+        &self,
+        game: &mut GameBoard,
+        // need to put a power plant price map somewhere
+        resources: Vec<(Currency, Price)>,
+        location: (u64, u64),
+        plant_type: PlantType,
+        owner: EntityID,
+    ) {
+        // check whether owner has enough of each material
+        // there's a better way to do this
+        let mut insufficient = false;
+        for (currency, price) in resources {
+            let token = &mut game.tokens[currency];
+            token.transaction();
+            if token.balance_check(&owner) < price {
+                insufficient = true;
+            }
+            token.end_transaction();
+        }
+        if insufficient {
+            return;
+        }
+        // create base nft?
+        let base_power_plant = BaseNFT {
+            owner,
+            nft_id: game.alloc(),
+            transfer_count: 0,
+        };
+        // insert into registry and get pointer
+        let plant_ptr = game.nfts.add(Box::new(base_power_plant));
+        // create PowerPlant nft
+        let new_plant = self.new(plant_ptr, plant_type, location);
+        // add to plant register, need to return Plant?
+        let _ = game.nfts.power_plants.insert(plant_ptr, new_plant).unwrap();
+
+        // exchange (or burn?) tokens
+        for (currency, price) in resources {
+            let token = &mut game.tokens[currency];
+            token.transaction();
+            let _ = token.transfer(&owner, &plant_ptr.0, price);
+            token.end_transaction();
         }
     }
     /// Compute the total hashes per second of this powerplant at this game state
