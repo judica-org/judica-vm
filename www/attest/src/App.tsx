26578522c1615104582104c8ff532f8a57494bb9
Jeremy Rubin <j@rubin.io>
2022-09-15 17:27:57 -0700

    Basic DataGrid-ization of attest tables

--- www/attest/src/App.tsx
+++ www/attest/src/App.tsx
@@ -1,6 +1,9 @@
 import React, { FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DataGrid, GridActionsCellItem, GridColDef, GridColumns, GridRowParams, GridRowsProp, GridToolbarContainer } from '@mui/x-data-grid';
+import { Cancel, Create, Newspaper, Report, Start } from '@mui/icons-material';
+import { Button, Container } from '@mui/material';
 
 
 function App() {
@@ -44,26 +47,48 @@ function App() {
   };
   return (
     <div className="App" >
-      Connected to: {url}
-      <form onSubmit={handle_click}>
-        <input ref={service} name="service" type="text" ></input>
-        <button type="submit">Set Server</button>
-      </form>
-      <hr></hr>
-      {url && <AddPeer root={url}></AddPeer>}
-      <hr></hr>
-      {url && <MakeGenesis url={url}></MakeGenesis>}
-      <hr></hr>
-      {status && status.hidden_service_url && <div>
-        <h1>Tor Enabled:</h1>
-        <h2>{status.hidden_service_url[0]}:{status.hidden_service_url[1]} </h2>
-      </div>}
-      {status && <Peers peers={status.peers}></Peers>}
-      {status && <TaskSet tasks={status.peer_connections}></TaskSet>}
-      {status && <Tips tips={status.tips}></Tips>}
-      {status && url && <Users users={status.all_users} url={url}></Users>}
-      {url && <ExpensiveMsgDB url={url}></ExpensiveMsgDB>}
+      <Container maxWidth={"lg"}>
 
+        <div className="TableGrid">
+          <div style={{ gridArea: "head" }}>
+            Connected to: {url}
+            <form onSubmit={handle_click}>
+              <input ref={service} name="service" type="text" ></input>
+              <button type="submit">Set Server</button>
+            </form>
+            <hr></hr>
+            {url && <AddPeer root={url}></AddPeer>}
+            <hr></hr>
+            {url && <MakeGenesis url={url}></MakeGenesis>}
+            <hr></hr>
+            {status && status.hidden_service_url && <div>
+              <h1>Tor Enabled:</h1>
+              <h6>{status.hidden_service_url[0]}:{status.hidden_service_url[1]} </h6>
+            </div>}
+          </div>
+
+          <div style={{ gridArea: "a" }}>
+            <h4>Peers</h4>
+            {status && <Peers peers={status.peers}></Peers>}
+          </div>
+          <div style={{ gridArea: "b" }}>
+            <h4>Tasks</h4>
+            {status && <TaskSet tasks={status.peer_connections}></TaskSet>}
+          </div>
+          <div style={{ gridArea: "c" }}>
+            <h4>Tips</h4>
+            {status && <Tips tips={status.tips}></Tips>}
+          </div>
+          <div style={{ gridArea: "d" }}>
+            <h4>Key Chain</h4>
+            {status && url && <Users users={status.all_users} url={url}></Users>}
+          </div>
+          <div style={{ gridArea: "e" }}>
+            <h4>DB Snapshot</h4>
+            {url ? <ExpensiveMsgDB url={url}></ExpensiveMsgDB> : <div></div>}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
@@ -86,49 +111,48 @@ function MakeGenesis(props: { url: String }) {
   return <button onClick={() => handle()}>New Genesis</button>;
 }
 
-function NewMsg(props: { url: String, pk: String }) {
-  const handle = async () => {
-    const message = window.prompt("DANGER: Invalid message May Corrupt Your Chain.\n\nWhat message should we send?");
-    if (message) {
-      let js = JSON.parse(message);
-      const c = window.confirm(`Are you sure? Pushing: \n ${JSON.stringify(message)}`);
-      if (!c) return;
-      const ret = fetch(`${props.url}/push_message_dangerous`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ msg: js, key: props.pk })
-      })
-      console.log(await (await ret).json());
+const handle_new_msg = async (url: string, pk: string) => {
+  const message = window.prompt("DANGER: Invalid message May Corrupt Your Chain.\n\nWhat message should we send?");
+  if (message) {
+    let js = JSON.parse(message);
+    const c = window.confirm(`Are you sure? Pushing: \n ${JSON.stringify(message)}`);
+    if (!c) return;
+    const ret = fetch(`${url}/push_message_dangerous`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ msg: js, key: pk })
+    })
+    console.log(await (await ret).json());
+  }
+};
+
+function Users(props: { users: Array<[string, string, boolean]>, url: string }) {
+
+  const rows: GridRowsProp = props.users.map(([pubkey, nickname, has_private_key], id) => {
+    return { id, pubkey, nickname, has_private_key }
+  });
+  const columns = [
+    { field: 'pubkey', headerName: 'Public Key', minWidth: 300 },
+    { field: 'nickname', headerName: 'Nickname', minWidth: 150 },
+    { field: 'has_private_key', headerName: 'Known Secret Key', minWidth: 150 },
+    {
+      headerName: 'New Message',
+      field: 'actions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem icon={<Create></Create>} onClick={() =>
+          handle_new_msg(props.url, rows[params.id as number].pubkey)} label="Create New Message" />,
+      ]
     }
-  };
-  return <button onClick={() => handle()}>Create Message</button>;
-}
+  ];
 
-function Users(props: { users: Array<[String, String, boolean]>, url: String }) {
+  return <DataGrid rows={rows} columns={columns} />;
 
-  const rows = props.users.map(([pubkey, nickname, has_priv]) => <tr>
-    <td>{pubkey}</td>
-    <td>{nickname}</td>
-    <td>{has_priv ? "Yes" : "No"}</td>
-    <td>{has_priv && <NewMsg url={props.url} pk={pubkey}></NewMsg>}</td>
-  </tr>);
-  return <table>
-    <thead>
-      <tr>
-        <th>Key</th>
-        <th>Nickname</th>
-        <th>Private Key Known?</th>
-        <th>Push Message</th>
-      </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>
 }
-function Tips(props: { tips: Array<{ envelope: { header: { key: string, ancestors?: { genesis: string }, height: string }, msg: any }, hash: string }> }) {
+type Envelope = { header: { key: string, ancestors?: { genesis: string }, height: string }, msg: any };
+function Tips(props: { tips: Array<{ envelope: Envelope, hash: string }> }) {
   const [view_flash, flash] = React.useState<null | string>(null);
   React.useEffect(() => {
     const t = setTimeout(() => view_flash && flash(null), 1000);
@@ -139,39 +163,35 @@ function Tips(props: { tips: Array<{ envelope: { header: { key: string, ancestor
       navigator.clipboard.writeText(s);
     }
   }
-  const rows = props.tips.map((x) => {
-    const genesis = x.envelope.header.ancestors?.genesis ?? x.hash;
-    const msg = JSON.stringify(x.envelope.msg);
-    return <tr key={x.hash}>
-      <td onClick={copy_on_click(genesis)} title="Click to Copy Genesis Hash">{genesis.substring(0, 16)}...</td>
-      <td onClick={copy_on_click(x.envelope.header.key)} title="Click to Copy Key">{x.envelope.header.key.substring(0, 16)}...</td>
-      <td onClick={copy_on_click(x.hash)} title="Click to Copy Message Hash">{x.hash.substring(0, 16)}...</td>
-      <td>{x.envelope.header.height}</td>
-      <td onClick={copy_on_click(msg)} title="Click to Copy Message">{msg.substring(0, 20)}{msg.length > 20 && <span>...</span>}</td>
-      <td><button onClick={() => console.log(x.envelope)}>log msg</button></td>
-    </tr>
+
+  const rows: GridRowsProp = props.tips.map((tip, id) => {
+    const genesis = tip.envelope.header.ancestors?.genesis ?? tip.hash;
+    const msg = JSON.stringify(tip.envelope.msg);
+    const signing_key = tip.envelope.header.key;
+    return { id, genesis, msg, signing_key, hash: tip.hash, height: tip.envelope.header.height }
   });
-  return <div>
-    <h4>{view_flash || "Click Field Below to Copy"}</h4>
-    <table>
-      <thead>
-        <tr>
-          <th>Genesis</th>
-          <th>Key</th>
-          <th>Msg Hash</th>
-          <th>Height</th>
-          <th>Msg</th>
-          <th>To Console</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table></div>
+  const columns = [
+    { field: 'genesis', headerName: 'Genesis', width: 300 },
+    { field: 'signing_key', headerName: 'Public Key', width: 300 },
+    { field: 'hash', headerName: 'Message Hash', width: 150 },
+    { field: 'height', headerName: 'Height', width: 150 },
+    { field: 'msg', headerName: 'Message', width: 150 },
+    {
+      headerName: 'Log Message',
+      field: 'actions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem icon={<Report></Report>} onClick={() =>
+          console.log(["message"], JSON.parse(rows[params.id as number].msg))} label="Log Message" />
+      ]
+    }
+  ];
+
+  return <DataGrid rows={rows} columns={columns} />;
 }
 
 function ExpensiveMsgDB(props: { url: string }) {
-  const [data, set_data] = React.useState({});
+  const [data, set_data] = React.useState<Record<string, Envelope>>({});
   const [text, set_text] = React.useState("");
   const handle = async () => {
     const target = `${props.url}/expensive_db_snapshot`;
@@ -183,77 +203,92 @@ function ExpensiveMsgDB(props: { url: string }) {
     }
     catch { }
   };
-  const rows = Object.entries(data).map(([k, envelope]: [string, any]) => <tr key={k}>
-    <td>{k.substring(0, 16)}</td>
-    <td>{envelope.header.ancestors?.genesis.substring(0, 16) ?? ""}</td>
-    <td>{envelope.header.height}</td>
-    <td onMouseOver={()=>set_text(JSON.stringify(envelope.msg))}>{JSON.stringify(envelope.msg).substring(0, 20)}</td>
-    <td><button onClick={() => console.log(envelope)}>log msg</button></td>
-  </tr>);
-  return <div>
-    <button onClick={handle}>Refresh</button>
-    <div style={{"display":"grid", "gridTemplateColumns": "1fr 1fr"}}>
-      <table>
-        <thead>
-          <tr>
-            <th>Msg Hash</th>
-            <th>Genesis</th>
-            <th>Height</th>
-            <th>Msg</th>
-            <th>To Console</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-      <textarea contentEditable={false} value={text}></textarea>
-    </div>
-  </div>
+
+  const rows: GridRowsProp = Object.entries(data).map(([msg_hash, envelope]: [string, Envelope], id) => {
+    const genesis = envelope.header.ancestors?.genesis ?? msg_hash;
+    const msg = JSON.stringify(envelope.msg);
+    const signing_key = envelope.header.key;
+    return { id, genesis, msg, signing_key, hash: msg_hash, height: envelope.header.height }
+  });
+  const columns = [
+    { field: 'genesis', headerName: 'Genesis', width: 300 },
+    { field: 'signing_key', headerName: 'Public Key', width: 300 },
+    { field: 'hash', headerName: 'Message Hash', width: 150 },
+    { field: 'height', headerName: 'Height', width: 150 },
+    { field: 'msg', headerName: 'Message', width: 150 },
+    {
+      headerName: 'Log Message',
+      field: 'actions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem icon={<Report></Report>} onClick={() =>
+          console.log(["message"], JSON.parse(rows[params.id as number].msg))} label="Log Message" />
+      ]
+    }
+  ];
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <h4>All Messages in DB</h4>
+        <Button onClick={handle}> Refresh </Button>
+      </GridToolbarContainer>
+    );
+  }
+
+  return <DataGrid rows={rows} columns={columns} components={{ Toolbar: CustomToolbar }} />;
 }
 
 
 function Peers(props: { peers: Array<{ service_url: string, port: string, fetch_from: boolean, push_to: boolean }> }) {
 
-  const rows = props.peers.map(({ service_url, port, fetch_from, push_to }) => <tr key={`${service_url}:${port}`}>
-    <td>{service_url}</td>
-    <td>{port}</td>
-    <td>{fetch_from ? "Enabled" : "Disabled"}</td>
-    <td>{push_to ? "Enabled" : "Disabled"}</td>
-  </tr>);
-  return <table>
-    <thead>
-      <tr>
-        <th>Host</th>
-        <th>Port</th>
-        <th>Fetch</th>
-        <th>Push</th>
-      </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>
+  const rows: GridRowsProp = props.peers.map((peer, id) => {
+    const row: typeof rows[number] = Object.fromEntries(Object.entries(peer));
+    row.id = id;
+    return row;
+  });
+  const columns = [
+    { field: 'service_url', headerName: 'Host', minWidth: 100 },
+    { field: 'port', headerName: 'Port', minWidth: 100 },
+    { field: 'fetch_from', headerName: 'Fetch', minWidth: 50 },
+    {
+      headerName: 'Fetch Actions',
+      field: 'fetch_pactions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem icon={<Cancel></Cancel>} onClick={() =>
+          console.log(["Peer"], "TODO")} label="Log Message" />,
+        <GridActionsCellItem icon={<Start></Start>} onClick={() =>
+          console.log(["Peer"], "TODO")} label="Log Message" />
+      ]
+    },
+    { field: 'push_to', headerName: 'Push', width: 50 },
+    {
+      headerName: 'Push Actions',
+      field: 'push_actions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem icon={<Cancel></Cancel>} onClick={() =>
+          console.log(["Peer"], "TODO")} label="Log Message" />,
+        <GridActionsCellItem icon={<Start></Start>} onClick={() =>
+          console.log(["Peer"], "TODO")} label="Log Message" />
+      ]
+    }
+  ];
+
+  return <DataGrid rows={rows} columns={columns} />;
 }
 
 function TaskSet(props: { tasks: Array<[string, number, "Fetch" | "Push"]> }) {
-  const rows = props.tasks.map(([host, port, typ]) => <tr key={`${typ}://${host}:${port}`}>
-    <td>{typ}</td>
-    <td>{host}</td>
-    <td>{port}</td>
-  </tr>);
-  return <table>
-    <thead>
-      <tr>
-        <th>Task Type</th>
-        <th>Host</th>
-        <th>Port</th>
-      </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-  </table>
+  const rows: GridRowsProp = props.tasks.map(([host, port, typ], id) => {
+    return { id, host, port, typ };
+  });
+  const columns = [
+    { field: 'host', headerName: 'Host', minWidth: 100 },
+    { field: 'port', headerName: 'Port', minWidth: 100 },
+    { field: 'typ', headerName: 'Type', minWidth: 50 },
+  ];
+
+  return <DataGrid rows={rows} columns={columns} />;
 }
 
 function AddPeer(props: { root: string }) {
