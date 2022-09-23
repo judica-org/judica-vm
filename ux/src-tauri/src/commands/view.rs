9086975b9665be57f974d047740ceb0d9eb2c351
Jeremy Rubin <j@rubin.io>
2022-09-22 18:19:56 -0700

    Refactor inner loop into an inner fnction

--- ux/src-tauri/src/commands/view.rs
+++ ux/src-tauri/src/commands/view.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::{Database, Game, GameState, PrintOnDrop, SigningKeyInner};
 
 use mine_with_friends_board::nfts::{
@@ -5,7 +7,7 @@ use mine_with_friends_board::nfts::{
 };
 use sapio_bitcoin::XOnlyPublicKey;
 
-use tauri::{State, Window};
+use tauri::{async_runtime::Mutex, State, Window};
 use tokio::sync::futures::Notified;
 use tracing::info;
 
@@ -18,105 +20,110 @@ pub(crate) async fn game_synchronizer_inner(
     info!("Registering Window for State Updates");
     let _p = PrintOnDrop("Registration Canceled".into());
     loop {
-        // No Idea why the borrow checker likes this, but it seems to be the case
-        // that because the notified needs to live inside the async state machine
-        // hapily, giving a stable reference to it tricks the compiler into thinking
-        // that the lifetime is 'static and we can successfully wait on it outside
-        // the lock.
-        let mut arc_cheat = None;
-        let signing_key: Option<_> = *signing_key.inner().lock().await;
-        let (gamestring, wait_on, key, chat_log, user_inventory) = {
-            let mut game = s.inner().lock().await;
-            let s = game
-                .as_ref()
-                .map(|g| serde_json::to_string(&g.board).unwrap_or("null".into()))
-                .unwrap_or("null".into());
-            arc_cheat = game.as_ref().map(|g: &Game| g.should_notify.clone());
-            let w: Option<Notified> = arc_cheat.as_ref().map(|x| x.notified());
-            let chat_log = game
-                .as_ref()
-                .map(|g| g.board.get_ux_chat_log())
-                .unwrap_or_default();
-            let user_inventory = game
-                .as_mut()
-                .map(|g| {
-                    g.board
-                        .get_ux_user_inventory(signing_key.unwrap().to_string())
-                })
-                .unwrap()
-                .unwrap();
-            (
-                s,
-                w,
-                game.as_ref().map(|g| g.host_key),
-                chat_log,
-                user_inventory,
-            )
-        };
-        let (appName, prefix, list_of_chains, user_keys) = {
-            let l = d.inner().state.lock().await;
-            if let Some(g) = l.as_ref() {
-                let handle = g.db.get_handle().await;
-                let v = handle.get_all_users().map_err(|_| ())?;
-                let keys: Vec<XOnlyPublicKey> = handle.get_keymap().unwrap().into_keys().collect();
-                (g.name.clone(), g.prefix.clone(), v, keys)
-            } else {
-                ("".into(), None, vec![], vec![])
-            }
-        };
-
-        // TODO: move these under a single held game lock
-        // Attempt to get data to show prices
-        let raw_price_data = {
-            let mut game = s.inner().lock().await;
-            let p = game
-                .as_mut()
-                .map(|g| g.board.get_ux_materials_prices())
-                .unwrap_or(Ok(Vec::new()))
-                .unwrap();
-            p
-        };
-
-        let power_plants = {
-            let mut game = s.inner().lock().await;
-            let plants: Vec<(NftPtr, UXPlantData)> = game
-                .as_mut()
-                .map(|g| g.board.get_ux_power_plant_data())
-                .unwrap_or_else(Vec::new);
-            plants
-        };
-
-        let listings = {
-            let game = s.inner().lock().await;
-            let listings = game
-                .as_ref()
-                .map(|g| g.board.get_ux_energy_market())
-                .unwrap_or(Ok(UXForSaleList {
-                    listings: Vec::new(),
-                }))
-                .unwrap();
-            listings
-        };
-
-        info!("Emitting State Updates");
-        window.emit("available-sequencers", list_of_chains);
-
-        window.emit("chat-log", chat_log);
-        window.emit("signing-key", signing_key);
-        window.emit("host-key", key).unwrap();
-        window.emit("user-keys", user_keys).unwrap();
-        window.emit("db-connection", (appName, prefix)).unwrap();
-        window.emit("game-board", gamestring).unwrap();
-        window.emit("materials-price-data", raw_price_data).unwrap();
-        window.emit("power-plants", power_plants).unwrap();
-        window.emit("energy-exchange", listings.listings).unwrap();
-        window.emit("user-inventory", user_inventory).unwrap();
-        if let Some(w) = wait_on {
-            w.await;
-        } else {
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        }
+        game_synchronizer_inner_loop(&signing_key.inner(), &s.inner(), &d.inner(), &window).await?;
     }
+}
+
+async fn game_synchronizer_inner_loop(
+    signing_key: &Arc<Mutex<Option<XOnlyPublicKey>>>,
+    s: &Arc<Mutex<Option<Game>>>,
+    d: &Database,
+    window: &Window,
+) -> Result<(), ()> {
+    // No Idea why the borrow checker likes this, but it seems to be the case
+    // that because the notified needs to live inside the async state machine
+    // hapily, giving a stable reference to it tricks the compiler into thinking
+    // that the lifetime is 'static and we can successfully wait on it outside
+    // the lock.
+    let mut arc_cheat = None;
+    let signing_key: Option<_> = *signing_key.lock().await;
+    let (gamestring, wait_on, key, chat_log, user_inventory) = {
+        let mut game = s.lock().await;
+        let s = game
+            .as_ref()
+            .map(|g| serde_json::to_string(&g.board).unwrap_or("null".into()))
+            .unwrap_or("null".into());
+        arc_cheat = game.as_ref().map(|g: &Game| g.should_notify.clone());
+        let w: Option<Notified> = arc_cheat.as_ref().map(|x| x.notified());
+        let chat_log = game
+            .as_ref()
+            .map(|g| g.board.get_ux_chat_log())
+            .unwrap_or_default();
+        let user_inventory = game
+            .as_mut()
+            .map(|g| {
+                g.board
+                    .get_ux_user_inventory(signing_key.unwrap().to_string())
+            })
+            .unwrap()
+            .unwrap();
+        (
+            s,
+            w,
+            game.as_ref().map(|g| g.host_key),
+            chat_log,
+            user_inventory,
+        )
+    };
+    let (appName, prefix, list_of_chains, user_keys) = {
+        let l = d.state.lock().await;
+        if let Some(g) = l.as_ref() {
+            let handle = g.db.get_handle().await;
+            let v = handle.get_all_users().map_err(|_| ())?;
+            let keys: Vec<XOnlyPublicKey> = handle.get_keymap().unwrap().into_keys().collect();
+            (g.name.clone(), g.prefix.clone(), v, keys)
+        } else {
+            ("".into(), None, vec![], vec![])
+        }
+    };
+    // TODO: move these under a single held game lock
+    // Attempt to get data to show prices
+    let raw_price_data = {
+        let mut game = s.lock().await;
+        let p = game
+            .as_mut()
+            .map(|g| g.board.get_ux_materials_prices())
+            .unwrap_or(Ok(Vec::new()))
+            .unwrap();
+        p
+    };
+    let power_plants = {
+        let mut game = s.lock().await;
+        let plants: Vec<(NftPtr, UXPlantData)> = game
+            .as_mut()
+            .map(|g| g.board.get_ux_power_plant_data())
+            .unwrap_or_else(Vec::new);
+        plants
+    };
+    let listings = {
+        let game = s.lock().await;
+        let listings = game
+            .as_ref()
+            .map(|g| g.board.get_ux_energy_market())
+            .unwrap_or(Ok(UXForSaleList {
+                listings: Vec::new(),
+            }))
+            .unwrap();
+        listings
+    };
+    info!("Emitting State Updates");
+    window.emit("available-sequencers", list_of_chains);
+    window.emit("chat-log", chat_log);
+    window.emit("signing-key", signing_key);
+    window.emit("host-key", key).unwrap();
+    window.emit("user-keys", user_keys).unwrap();
+    window.emit("db-connection", (appName, prefix)).unwrap();
+    window.emit("game-board", gamestring).unwrap();
+    window.emit("materials-price-data", raw_price_data).unwrap();
+    window.emit("power-plants", power_plants).unwrap();
+    window.emit("energy-exchange", listings.listings).unwrap();
+    window.emit("user-inventory", user_inventory).unwrap();
+    if let Some(w) = wait_on {
+        w.await;
+    } else {
+        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    }
+    Ok(())
 }
 
 pub(crate) async fn list_my_users_inner(
@@ -147,7 +154,9 @@ pub(crate) async fn super_mint_power_plant_cost(
     signing_key: State<'_, SigningKeyInner>,
 ) -> Result<u128, ()> {
     let cost = {
-        let cost_detail = mint_power_plant_cost(scale, location, plant_type, s, signing_key).await.unwrap();
+        let cost_detail = mint_power_plant_cost(scale, location, plant_type, s, signing_key)
+            .await
+            .unwrap();
         let btc_costs: Vec<u128> = cost_detail.iter().map(|(_, _, btc)| *btc).collect();
         btc_costs.iter().sum()
     };
