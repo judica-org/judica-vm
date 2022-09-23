747da3e904d02112e07ef50fe743a2a63c24b6ec
Jeremy Rubin <j@rubin.io>
2022-09-23 16:39:20 -0700

    Chat form cleanup

--- ux/src/chat/NewChat.tsx
+++ ux/src/chat/NewChat.tsx
@@ -1,18 +1,20 @@
+import { Button, FormControl, TextField } from "@mui/material";
 import React from "react";
 import { tauri_host } from "../tauri_host";
 
 export function NewChat() {
     const [text, set_text] = React.useState<string>("");
-    const newLocal = (ev: React.FormEvent<HTMLFormElement>): void => {
+    const newLocal = (ev: React.FormEvent<HTMLDivElement>): void => {
+
         ev.preventDefault();
         tauri_host.send_chat(text);
         set_text("");
+
     };
     return <div className="NewMessage">
-        <form onSubmit={newLocal}>
-            <label>New Message</label>
-            <input type="textarea" onChange={(ev) => set_text(ev.target.value)}></input>
-            <button type="submit">Send</button>
-        </form>
+        <FormControl onSubmit={newLocal}>
+            <TextField label="New Message" type="textarea" onChange={(ev) => set_text(ev.target.value)} value={text}></TextField>
+            <Button type="submit">Send</Button>
+        </FormControl>
     </div>
 }
