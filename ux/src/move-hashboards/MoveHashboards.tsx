a2d96a4f30a73b8d3784fbf56073900d786b40ad
Caroline Ponzini <cara@Carolines-MacBook-Air.local>
2022-09-28 11:05:11 -0700

    for to move hashboards

---
+++ ux/src/move-hashboards/MoveHashboards.tsx
@@ -0,0 +1,48 @@
+import { Card, CardHeader, CardContent, Typography, FormControl, TextField, Button } from "@mui/material";
+import { useState } from "react";
+import { UserPowerPlant } from "../App";
+import { tauri_host } from "../tauri_host";
+import { MoveHashboardsActions } from "../util";
+
+export const MoveHashboards = ({ action: initial_action, plant, user_hashboards, hashboard_pointer }:
+  { readonly action: MoveHashboardsActions, plant: UserPowerPlant, user_hashboards: number, hashboard_pointer: string }) => {
+  const [action, set_action] = useState<MoveHashboardsActions>(initial_action);
+  const [hashboard_qty, set_hashboard_qty] = useState<number | null>(null);
+  const switch_action = () => {
+    switch (action) {
+      case "ADD":
+        return ("REMOVE")
+      case "REMOVE":
+        return ("ADD")
+    };
+  }
+
+  const handle_click = (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    // need the hashboard token id
+    ev.preventDefault();
+    if (hashboard_qty && action === "ADD")
+      tauri_host.make_move_inner({ send_tokens: { amount: hashboard_qty, currency: parseInt(hashboard_pointer), to: plant.id } }, "0");
+    if (hashboard_qty && action === "REMOVE")
+      tauri_host.make_move_inner({ remove_tokens: { nft_id: plant.id, amount: hashboard_qty, currency: parseInt(hashboard_pointer) } }, "0");
+  };
+
+  return <Card>
+    <CardHeader
+      title={action}
+      subheader={`${action === "ADD" ? user_hashboards : plant.miners} Hashboards Available`}
+    >
+    </CardHeader>
+    <CardContent>
+      <Typography variant="h6">
+        {action} hashboards {action === "ADD" ? 'to' : 'from'} PlantId: {plant.id}
+      </Typography>
+      <div className='MoveForm' >
+        <FormControl >
+          <TextField label={"Hashboards to move:"} type="number" value={hashboard_qty} onChange={(ev) => { set_hashboard_qty(parseInt(ev.target.value)) }}></TextField>
+          <Button type="submit" onClick={handle_click}>{action}</Button>
+          <Button onClick={() => set_action(switch_action())}>Switch To {switch_action()} </Button>
+        </FormControl>
+      </div>
+    </CardContent>
+  </Card>;
+};
