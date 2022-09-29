fe5e84976baf550373d7de0358053c112aeb404e
Jeremy Rubin <j@rubin.io>
2022-09-29 13:58:52 -0700

    Clean up game-host logic mildly

--- common/game-host/src/main.rs
+++ common/game-host/src/main.rs
@@ -4,7 +4,8 @@ use attest_messages::{
     Authenticated, CanonicalEnvelopeHash, Envelope, GenericEnvelope, WrappedJson,
 };
 use game_host_messages::{BroadcastByHost, Channelized};
-
+use sapio_bitcoin::secp256k1::rand;
+use sapio_bitcoin::secp256k1::rand::seq::SliceRandom;
 use sapio_bitcoin::secp256k1::All;
 use sapio_bitcoin::{secp256k1::Secp256k1, KeyPair};
 use serde::{Deserialize, Serialize};
@@ -118,19 +119,16 @@ async fn game(
     // can know which messages we've sequenced previously.
     {
         let handle = db.get_handle().await;
-        let v: Vec<Authenticated<GenericEnvelope<WrappedJson>>> =
+        let v: Vec<Authenticated<GenericEnvelope<Channelized<BroadcastByHost>>>> =
             handle.load_all_messages_for_user_by_key_connected(&oracle_publickey)?;
-        {
-            for x in v {
-                let d =
-                    serde_json::from_value::<Channelized<BroadcastByHost>>(x.msg().clone().into())?;
-                match d.data {
-                    BroadcastByHost::Sequence(l) => already_sequenced.extend(l.iter()),
-                    BroadcastByHost::NewPeer(_) => {}
-                    BroadcastByHost::Heartbeat => {}
-                    BroadcastByHost::GameSetup(_) => {}
-                }
-            }
+        let sequencer = game_sequencer::RawSequencer {
+            sequencer_envelopes: v,
+            msg_cache: Default::default(),
+        };
+        let sequencer: game_sequencer::OfflineSequencer<game_player_messages::ParticipantAction> =
+            sequencer.try_into()?;
+        for batch in sequencer.batches_to_sequence {
+            already_sequenced.extend(batch.iter());
         }
     }
     info!(
@@ -141,6 +139,41 @@ async fn game(
     let mut message_by_genesis = HashMap::<CanonicalEnvelopeHash, BTreeMap<i64, Envelope>>::new();
     let mut next_height_to_sequence: HashMap<CanonicalEnvelopeHash, i64> = Default::default();
     let mut seq = 0;
+
+    // Get All the messages that we've not yet seen, but incosistently
+    // Incosistency means that we may still be fetching priors tips in our
+    // network stack.
+    //
+    // Only fetch the messages for our groups though
+    {
+        let handle = db.get_handle().await;
+        handle
+                .get_all_chain_commit_group_members_new_envelopes_for_chain_into_inconsistent::<Authenticated<Envelope>, WrappedJson>(
+                    oracle_publickey,
+                    &mut seq,
+                    &mut all_unprocessed_messages,
+                )?
+    }
+    // Only on the first pass, remove the messages that have already been sequenced
+    //
+    // Later passes will be cleared.
+    //
+    // Also record the prior max sequenced.
+    for m in already_sequenced.into_iter() {
+        if let Some(msg) = all_unprocessed_messages.remove(&m) {
+            let r = next_height_to_sequence
+                .entry(msg.get_genesis_hash())
+                .or_default();
+            *r = std::cmp::max(msg.header().height() + 1, *r);
+        } else {
+            debug!(message_hash=?m, "Missing Message!");
+            panic!("Message was sequenced, but was not contained in database");
+        }
+    }
+    info!(
+        n = next_height_to_sequence.len(),
+        "Found Existing Comitted Chains"
+    );
     loop {
         // Get All the messages that we've not yet seen, but incosistently
         // Incosistency means that we may still be fetching priors tips in our
@@ -156,26 +189,6 @@ async fn game(
                     &mut all_unprocessed_messages,
                 )?
         }
-        // Only on the first pass, remove the messages that have already been sequenced
-        //
-        // Later passes will be cleared.
-        //
-        // Also record the prior max sequenced.
-        {
-            for m in &already_sequenced {
-                if let Some(msg) = all_unprocessed_messages.remove(m) {
-                    let r = next_height_to_sequence
-                        .entry(msg.get_genesis_hash())
-                        .or_default();
-                    *r = std::cmp::max(msg.header().height() + 1, *r);
-                }
-            }
-            info!(
-                n = next_height_to_sequence.len(),
-                "Found Existing Comitted Chains"
-            );
-            already_sequenced.clear();
-        }
         info!(
             n = all_unprocessed_messages.len(),
             "Got New Messages to Sequence"
@@ -188,7 +201,7 @@ async fn game(
                 // we can remove it now because the only reason we will drop it is if it is not to be sequenced
                 if let Some((_k, e)) = all_unprocessed_messages.remove_entry(value) {
                     if e.header().key() == oracle_publickey {
-                        continue;
+                        panic!("Chain Cannot Listen to itself, logic error");
                     }
                     if message_by_genesis
                         .entry(e.get_genesis_hash())
@@ -208,28 +221,65 @@ async fn game(
         for (genesis, ms) in message_by_genesis.iter_mut() {
             let next_height = next_height_to_sequence.entry(*genesis).or_default();
             let first = *next_height;
+
+            if ms.keys().next() != Some(&first) {
+                continue;
+            }
             // iterate over the keys, checking for contiguity and breaking at any gap
-            for k in ms.keys() {
-                if *k == *next_height {
-                    *next_height += 1;
+            'scan_keys: for (k1, k2) in ms
+                .keys()
+                .zip(ms.keys().skip(1).chain(std::iter::once(&i64::MAX)))
+            {
+                if k2 - k1 == 1 {
                 } else {
-                    break;
+                    // minimally, the first (contained) +1. or the last next-element
+                    *next_height = k1 + 1;
+                    break 'scan_keys;
                 }
             }
-            // go from the first (e.g. 0)  to th
+            // go from the first (e.g. 0)  to the
             for k in first..*next_height {
-                to_sequence.push_back(
-                    ms.remove(&k)
-                        .expect("Must be present")
-                        .canonicalized_hash_ref(),
-                );
+                let e = ms.remove(&k).expect("Must be present");
+                to_sequence.push_back((
+                    e.header().height(),
+                    e.canonicalized_hash_ref(),
+                    e.get_genesis_hash(),
+                ));
             }
         }
 
         info!(key=?keypair.x_only_public_key().0, n=to_sequence.len(), "Messages to Sequence");
         if !to_sequence.is_empty() {
+            // to_sequence is naturally sorted by height here, which is a safe option.
+            // However, for more fairness, we should randomize and ensure height sortedness later
+
+            // ensure sorted by height
+            let mut group_min_height = to_sequence[0].0;
+            let mut g = to_sequence[0].2;
+            // To sequence is grouped by genesis. It will look like:
+            // g1@n g1@n+1 g2@m g3@n g3@n+1 g3@n+2
+            // therefore we can detect groups by where the discontinuities are.
+            // normalizes all the height groups...
+            // g1@0 g1@1 g2@0 g3@0 g3@1 g3@2
+            for s in to_sequence.iter_mut() {
+                if s.2 == g {
+                    s.0 -= group_min_height;
+                } else {
+                    group_min_height = s.0;
+                    s.0 -= group_min_height;
+                    g = s.2;
+                }
+            }
+
+            let slice = to_sequence.make_contiguous();
+            shuffle_slice(slice);
+
+            // schedules one message per chain in a randomized round-robin,
+            // balancing fairness and randomization.
+            to_sequence.as_mut_slices().0.sort_by_key(|k| k.0);
+
             let msg = Channelized {
-                data: BroadcastByHost::Sequence(to_sequence),
+                data: BroadcastByHost::Sequence(to_sequence.iter().map(|k| k.1).collect()),
                 channel: "default".into(),
             };
             let mut handle = db.get_handle().await;
@@ -252,4 +302,9 @@ async fn game(
             )?;
         }
     }
+}
+
+fn shuffle_slice<T>(to_sequence: &mut [T]) {
+    let mut rng = rand::thread_rng();
+    SliceRandom::shuffle(to_sequence, &mut rng);
 }
