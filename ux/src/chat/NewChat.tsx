2dd00036b3021a1084c281aee4dbb1ac4cbb73da
Jeremy Rubin <j@rubin.io>
2022-10-01 18:39:26 -0700

    Make Chat Text Field expand to multiline, make sending happen on enter

--- ux/src/chat/NewChat.tsx
+++ ux/src/chat/NewChat.tsx
@@ -1,21 +1,32 @@
-import { Button, FormControl, TextField } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import { Button, FormControl, FormGroup, IconButton, TextField } from "@mui/material";
 import React from "react";
 import { tauri_host } from "../tauri_host";
 
 export function NewChat() {
     const [text, set_text] = React.useState<string>("");
-    const newLocal = (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const press_send = (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
         console.log("Sending ", text);
         tauri_host.send_chat(text);
         set_text("");
 
     };
+    const press_enter = (ev: React.KeyboardEvent<HTMLDivElement>): void => {
+        if (ev.key !== "Enter") return;
+        console.log("Sending ", text);
+        tauri_host.send_chat(text);
+        set_text("");
+    };
     return <div className="NewMessage">
         <FormControl>
-            <TextField label="New Message" type="textarea" onChange={(ev) => set_text(ev.target.value)} value={text}></TextField>
-            <Button type="submit" onClick={
-                newLocal
-            }>Send</Button>
+            <FormGroup row>
+                <TextField
+                    size="medium"
+                    multiline={text.length > 80}
+                    onKeyPress={press_enter}
+                    label="New Message" type="textarea" onChange={(ev) => set_text(ev.target.value)} value={text}></TextField>
+                <IconButton type="submit" onClick={press_send}><Send></Send></IconButton>
+            </FormGroup>
         </FormControl>
     </div>
 }
