b45e0ef9f9c2f26021fa35c31e65c08a289bb2d5
Jeremy Rubin <j@rubin.io>
2022-10-02 22:22:41 -0700

    restore working time extractor

--- common/sapio-litigator/src/universe/extractors/time.rs
+++ common/sapio-litigator/src/universe/extractors/time.rs
@@ -1,19 +1,22 @@
-use std::time::Duration;
+use event_log::{
+    connection::EventLog,
+    db_handle::accessors::{occurrence::ToOccurrence, occurrence_group::OccurrenceGroupID},
+};
+use std::{error::Error, sync::Arc, time::Duration};
+use tokio::sync::Notify;
 
-use futures::stream::BoxStream;
+use crate::Event;
 
-pub async fn deadline(unix_time: i64, tolerance: u64) -> BoxStream<'static, ()> {
-    Box::pin(futures::stream::unfold(false, move |sent| async move {
-        if sent {
-            None
-        } else {
-            loop {
-                if attest_util::now() > unix_time {
-                    break;
-                }
-                tokio::time::sleep(Duration::from_secs(tolerance)).await;
-            }
-            Some(((), true))
-        }
-    }))
+pub async fn time_event_extractor(
+    evlog: EventLog,
+    evlog_group_id: OccurrenceGroupID,
+    new_synthetic_event: Arc<Notify>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    loop {
+        tokio::time::sleep(Duration::from_secs(30)).await;
+        let accessor = evlog.get_accessor().await;
+        let o: &dyn ToOccurrence = &Event::SyntheticPeriodicActions(attest_util::now());
+        accessor.insert_new_occurrence_now_from(evlog_group_id, o)?;
+        new_synthetic_event.notify_one();
+    }
 }
