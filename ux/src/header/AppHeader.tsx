b8b4bf9fc72584bfe380ba6f61003fee9d0696ae
Jeremy Rubin <j@rubin.io>
2022-10-07 00:58:16 -0400

    Basic UX Refactors to header

--- ux/src/header/AppHeader.tsx
+++ ux/src/header/AppHeader.tsx
@@ -4,21 +4,30 @@ import { KeySelector, KeySelectorProps } from './KeySelector';
 import { MakeNewChain } from './MakeNewChain';
 import "./AppHeader.css";
 import React from 'react';
-import { Typography } from '@mui/material';
+import { FormControl, FormControlLabel, Switch, Typography } from '@mui/material';
 import { NewGame, NewGameProps } from './NewGame';
 
 export function AppHeader({ db_name_loaded, which_game_loaded, available_sequencers, available_keys, signing_key, join_code, join_password }: {
   db_name_loaded: [string, null | string] | null
 
 } & SwitchToGameProps & KeySelectorProps & NewGameProps) {
+  const [new_or_old, set_new_or_old] = React.useState(false);
+  const action = new_or_old ? "New" : "Existing";
   return <div className="App-header">
     <Typography variant='h2'>1</Typography>
     <SwitchToDB {...{ db_name_loaded }}></SwitchToDB>
     {db_name_loaded &&
       <>
-        <Typography variant='h2'>2</Typography>
-        <NewGame {...{ join_code, join_password }}></NewGame>
-        <SwitchToGame {...{ available_sequencers, which_game_loaded }} ></SwitchToGame>
+        <FormControl>
+
+          <Typography variant='h2'>2</Typography>
+          <FormControlLabel control={
+            <Switch value={new_or_old} onClick={(a) => { set_new_or_old(!new_or_old) }}></Switch>
+          } label={`${action} Game`} />
+        </FormControl>
+        {new_or_old ?
+          <NewGame {...{ join_code, join_password }}></NewGame> :
+          <SwitchToGame {...{ available_sequencers, which_game_loaded }} ></SwitchToGame>}
       </>
     }
   </div >;
--- ux/src/header/NewGame.tsx
+++ ux/src/header/NewGame.tsx
@@ -1,22 +1,33 @@
-import { Button, FormControl, Slider, TextField, ToggleButton } from '@mui/material';
+import { Button, FormControl, FormControlLabel, Slider, Switch, TextField, ToggleButton, Typography } from '@mui/material';
 import { appWindow } from '@tauri-apps/api/window';
 import React from 'react';
 import { tauri_host } from '../tauri_host';
 
-export interface NewGameProps  {
-  join_code: string|null,
-  join_password: string|null
+export interface NewGameProps {
+  join_code: string | null,
+  join_password: string | null
 };
 export function NewGame(props: NewGameProps) {
   const [nick, set_nick] = React.useState<null | string>(null);
 
   const [join_or_new, set_join_or_new] = React.useState(false);
+  const action = join_or_new ? "Join" : "New";
   return <div>
-    <h6> New Player</h6>
+    <Typography variant='h6'>New Game</Typography>
     <FormControl >
-      <ToggleButton value={join_or_new} onChange={(a)=> {set_join_or_new(!join_or_new)}}></ToggleButton>
-      <TextField label='Nickname' onChange={(ev) => set_nick(ev.target.value)}></TextField>
-      <Button variant="contained" type="submit" onClick={(ev) => { ev.preventDefault(); nick && tauri_host.make_new_chain(nick) }}>New Game</Button>
+      <FormControlLabel control={
+        <Switch value={join_or_new} onClick={(a) => { set_join_or_new(!join_or_new) }}></Switch>
+      } label={`${action} Game`} />
+
+      <TextField label='Chain Nickname' onChange={(ev) => set_nick(ev.target.value)}></TextField>
+      {
+        join_or_new && <TextField label='Join Code' onChange={(ev) => set_nick(ev.target.value)}></TextField>
+      }
+      <Button variant="contained" type="submit" onClick={(ev) => { ev.preventDefault(); nick && tauri_host.make_new_chain(nick) }}>
+
+        {action} Game
+
+      </Button>
     </FormControl>
   </div>;
 }
