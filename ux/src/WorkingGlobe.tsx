d145d81882dd550b8bbb21e16ac616d311c75f1c
Caroline Ponzini <cara@Carolines-MacBook-Air.local>
2022-10-07 21:23:09 -0400

    remove power plant onhover

--- ux/src/WorkingGlobe.tsx
+++ ux/src/WorkingGlobe.tsx
@@ -154,16 +154,6 @@ export default () => {
                             el.style.cursor = 'pointer';
                             // set to 
                             el.onclick = () => PlantSelected(d.id);
-                            el.onmouseover = () => {
-                                el.innerHTML = `
-                                <b>ID: ${d.id}</b> <br />
-                                Owner: <i>${d.owner}</i> <br />
-                                Watts: <i>${d.watts}</i> <br />
-                                Hashrate: <i>${d.hashrate}</i> <br />
-                                ${d.for_sale ? 'For Sale' : ''}
-                                `
-                            }
-                            el.onmouseleave = () => el.innerHTML = svg;
                             return el;
                         }}
 
