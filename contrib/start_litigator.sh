db634f67b1a1bf82cda91828c24974e2b02c0562
Jeremy Rubin <j@rubin.io>
2022-10-10 16:06:47 -0400

    Fix sh scripts

--- contrib/start_litigator.sh
+++ contrib/start_litigator.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
-PIPENAME=$(mktemp -u)
-mkfifo -m 600 "$PIPENAME"
-export LITIGATOR_LOGFILE=$PIPENAME
-if [[ -z "${SEQUENCER_KEY}" ]]; then
-    echo "Paste in a key for the sequencer"
-    read -r SEQUENCER_KEY
-else
-    echo "Key Already Selected $SEQUENCER_KEY"
-fi
-export SEQUENCER_KEY
 export LITIGATOR_CONFIG_JSON=$(cat litigator_config.json.template | envsubst)
 
 echo $LITIGATOR_CONFIG_JSON | jq
