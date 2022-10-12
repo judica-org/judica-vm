066429486fe221ed92e2adce7e7f085803b48081
Jeremy Rubin <j@rubin.io>
2022-10-12 02:39:53 -0400

    Fix Shell Issue

--- contrib/build_wasm.sh
+++ contrib/build_wasm.sh
@@ -6,18 +6,13 @@ set -o pipefail
 
 case "$(uname -s)" in
 Darwin)
-
-    export PATH_BACK=$PATH
-    export CC_BACK=$CC
-    export AR_BACK=$AR
-
     export PATH="/usr/local/opt/llvm/bin:$PATH"
     export CC=/usr/local/opt/llvm/bin/clang
     export AR=/usr/local/opt/llvm/bin/llvm-ar
     if test -f "$CC"; then
         echo "Using CC=$CC AR=$AR PATH=$PATH"
     else
-        export PATH="/opt/homebrew/opt/llvm/bin:$PATH_BACK"
+        export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
         export CC=/opt/homebrew/opt/llvm/bin/clang
         export AR=/opt/homebrew/opt/llvm/bin/llvm-ar
         if test -f "$CC"; then
