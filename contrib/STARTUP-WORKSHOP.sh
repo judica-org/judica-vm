a4c83c3da1f41002e2a37b024714d2cf0854fac1
Jeremy Rubin <j@rubin.io>
2022-10-13 10:02:30 -0400

    Fix startup script

--- contrib/STARTUP-WORKSHOP.sh
+++ contrib/STARTUP-WORKSHOP.sh
@@ -55,7 +55,7 @@ else
     ;;
   esac
   # create a session with five panes
-  tmux new-session -d -s MySession -n "www" -d "$PWD/start_host_www.sh; /usr/bin/env $SHELL -i"
+  tmux new-session -d -s MySession -n "www"
   case "$USE_RELEASE_TAURI" in
   dev)
     tmux split-window -t MySession:0 "$PWD/start_tauri_front.sh; /usr/bin/env $SHELL -i"
@@ -69,29 +69,30 @@ else
     echo "Not Starting Tauri Frontend, Release Mode"
     ;;
   esac
-  tmux split-window -t MySession:0 "export PORTS=\"15533\"; $PWD/start_attest_www.sh; /usr/bin/env $SHELL -i"
-if [[ -n $START_HOST ]]; then
-  tmux split-window -t MySession:0 "export PORTS=\"15532\"; $PWD/start_attest_www.sh; /usr/bin/env $SHELL -i"
-fi
 
+  if [[ -n $START_HOST ]]; then
+    START_HOST_ADMINDB=15532
+  fi
+  export START_HOST_ADMINDB
+  tmux split-window -t MySession:0 "export PORTS=\"$START_HOST_ADMINDB\"; $PWD/start_attest_www.sh; /usr/bin/env $SHELL -i"
 
   # Player 1
-  tmux new-window -t MySession: -n "player-1" "export PLAYER=\"p1\" SOCKS_PORT=24402; $PWD/start_tauri.sh; /usr/bin/env $SHELL -i"
+  tmux new-window -t MySession: -n "player-1" "export PLAYER=\"p1\" SOCKS_PORT=24402 ADMINDB_PORT=15533; $PWD/start_tauri.sh; /usr/bin/env $SHELL -i"
   tmux split-window -t MySession:1 "export PLAYER=\"p1\" SOCKS_PORT=14458 APP_PORT=13329 CONTROL_PORT=15533; $PWD/start_attest.sh; /usr/bin/env $SHELL -i"
   tmux split-window -t MySession:1 "export PLAYER=\"p1\"; $PWD/start_litigator.sh; /usr/bin/env $SHELL -i"
 
-if [[ -n $START_HOST ]]; then
-  tmux new-window -t MySession: -n "host" "export PLAYER=\"host\"; $PWD/start_host.sh; /usr/bin/env $SHELL -i"
-  tmux split-window -t MySession:2 "export PLAYER=\"host\" SOCKS_PORT=14457 APP_PORT=13328 CONTROL_PORT=15532; $PWD/start_attest.sh; /usr/bin/env $SHELL -i"
-  tmux split-window -t MySession:2 "export PLAYER=\"host\"; $PWD/start_litigator.sh; /usr/bin/env $SHELL -i"
-fi
+  if [[ -n $START_HOST ]]; then
+    tmux new-window -t MySession: -n "host" "export PLAYER=\"host\"; $PWD/start_host.sh; /usr/bin/env $SHELL -i"
+    tmux split-window -t MySession:2 "export PLAYER=\"host\" SOCKS_PORT=14457 APP_PORT=13328 CONTROL_PORT=15532; $PWD/start_attest.sh; /usr/bin/env $SHELL -i"
+    tmux split-window -t MySession:2 "export PLAYER=\"host\"; $PWD/start_litigator.sh; /usr/bin/env $SHELL -i"
+  fi
 
   # change layout to tiled
   tmux select-layout -t MySession:0 tiled
   tmux select-layout -t MySession:1 tiled
-if [[ -n $START_HOST ]]; then
-  tmux select-layout -t MySession:2 tiled
-fi
+  if [[ -n $START_HOST ]]; then
+    tmux select-layout -t MySession:2 tiled
+  fi
 
   tmux attach -t MySession
 fi
